87c267d9890c393071709523ba589404f38044ac
ripunjayrai777 <ripunjayrai777@gmail.com>
2025-03-06 16:17:06 +0530

    function with object and array.. lect.20 completed

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -61,3 +61,58 @@ function loginUserMsg(username = "sam") {
 }
 
 console.log(loginUserMsg()); // this will print: sam! abhi login hua hai!
+
+// ************************     FUNCTIONS WITH OBJECTS AND ARRAYS *************************************************
+
+function calculatePrice(price) {
+  return price;
+} // here we create a simple function which is returning prince and we passes here one parameter
+console.log(calculatePrice(220)); // calling the function and give the one arguments, so this is working properly , what if we pass more than one argument ... like--
+
+//if we want to give one parameter and arguments more than one...then we should use rest operator (...)
+
+function calcuPrice(...value) {
+  //here ...value this is a rest operator
+  return value;
+}
+console.log(calcuPrice(100, 200, 455, 66)); //here all arguments will return in a single array ...like this-[ 100, 200, 455, 66 ]
+
+//other example of rest operator..
+function CalPra(num1, num2, ...num) {
+  return num;
+} //here again we define a CalPra function and give three parameters num1, num2 and ...num(rest operator) so here during call this function when we pass arguments then first argument will go in num1 and second argument will go in num2 and all other arguments will go in ...num (rest operator) ,and here we only returning ...num(rest operator) so
+
+console.log(CalPra(20, 40, 50, 60, 80, 50, 40)); //only ...rest operator arguments will printed in form of array - [ 50, 60, 80, 50, 40 ]
+
+//How object will pass in a functions....
+
+const user = {
+  usersname: "ripunjay",
+  price: 56,
+}; //here we are declare a object name user
+function handleObject(anyObject) {
+  //here we give the parameter to the function named anyObject we can change it..
+  console.log(
+    `Username is ${anyObject.usersname} and price is ${anyObject.price}`
+  );
+}
+handleObject(user); // here we will pass that object which data want to fetch in function handleObject.
+handleObject({
+  usersname: "sam",
+  price: 46,
+}); //we can also pass data like this , direct crete the object during function calling
+
+//How Array will pass in function....
+
+const newArry = [300, 200, 400, 6000, 5000];
+const newArry2 = [7000, 6000, 8000, 9000];
+function handleArr(getArray) {
+  // this function is taking a para meter name getArray, we can change it also
+  return getArray[1];
+} // here function is return the index number 1 value .
+
+console.log(handleArr(newArry)); //we will pass here array name during function call from where we want to fetch the data, this will return 200. (index 1)
+console.log(handleArr(newArry2)); //we will pass here array name during function call from where we want to fetch the data, this will return 6000. (index 1)
+
+//we can directly pass the array in to the function like this
+console.log(handleArr([45, 56, 23, 78, 89, 19])); // this will return 56 (index 1)
