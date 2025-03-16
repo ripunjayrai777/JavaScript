f3435970010417165a8332ae6d951441fd341555
ripunjayrai777 <ripunjayrai777@gmail.com>
2025-03-16 18:45:52 +0530

    control flow of JS ..lect 25 partially complete.

---
+++ 04_control_flow/one.js
@@ -0,0 +1,5 @@
+// if --
+const isUserLoggedIn = true;
+if(isUserLoggedIn){ // In the parenthesis, we will provide a condition which result is in Boolean, if true then scope will execute otherwise not.
+    console.log("User is Logged In.")
+}
