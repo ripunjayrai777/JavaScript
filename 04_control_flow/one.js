ae58f4d85dc1417bea1a6658dcce572ccf90341f
ripunjayrai777 <ripunjayrai777@gmail.com>
2025-03-17 10:32:34 +0530

    lect25.

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -3,3 +3,17 @@ const isUserLoggedIn = true;
 if(isUserLoggedIn){ // In the parenthesis, we will provide a condition which result is in Boolean, if true then scope will execute otherwise not.
     console.log("User is Logged In.")
 }
+//their are some comparison operater which we use at the place of condition..and return boolean result
+// <   - Less than 
+// >   - Greater than
+// <=  - Less than or Equal to 
+// >=  - Greater than or Equal to
+// !=  - Not Equal to
+// ==  - Is Equal to or not
+// === - Is Equal to or not, but it also check the data type
+// !== - Is Not Equal to , it also chech the sign.
+
+const temperature =41;
+if(temperature<50 ){
+    console.log("second run!!")
+}
