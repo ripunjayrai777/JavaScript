750362a7912460ab72f1261dca5b215738274f15
ripunjayrai777 <ripunjayrai777@gmail.com>
2025-03-18 07:23:51 +0530

    one.js in control_flow complete ..lect.26

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,7 +1,9 @@
 // if --
-const isUserLoggedIn = true;
-if(isUserLoggedIn){ // In the parenthesis, we will provide a condition which result is in Boolean, if true then scope will execute otherwise not.
+const isUserLoggedIn = 45;
+if(isUserLoggedIn>50){ // In the parenthesis, we will provide a condition which result is in Boolean, if true then scope will execute if condition is false then else block will execute.
     console.log("User is Logged In.")
+}else{ // Else block
+    console.log("elese block executed.")
 }
 //their are some comparison operater which we use at the place of condition..and return boolean result
 // <   - Less than 
@@ -13,7 +15,43 @@ if(isUserLoggedIn){ // In the parenthesis, we will provide a condition which res
 // === - Is Equal to or not, but it also check the data type
 // !== - Is Not Equal to , it also chech the sign.
 
-const temperature =41;
-if(temperature<50 ){
-    console.log("second run!!")
+const score = 200;
+if(score>100){ //this is false 
+    const power = "fly"; // we can use power only in this if scope, not at any other places.
+    console.log(`Power is : ${power}`); 
+}else{ //else block executed only when condition is false otherwise not execute and power will not give the error
+    console.log(`Power is destructured : ${power}`); // here we will get error: power is not defined.
+}
+
+
+//Short Hand Notation:
+
+const bal =1000;
+if (bal>500) console.log("This is one line scope without curly bracket, if we want to write more than one line than separate with comma"),
+console.log("this is second line") // this type of code writing is not recommended
+
+// Nested Conditions:
+
+if(bal<500){
+    console.log("less than 500")
+}else if(bal<750){
+    console.log("less than 750")
+}else if(bal<900){
+    console.log("less than 900")
+}else{
+    console.log("All if else condition is false, less than 1200")
+}
+
+//Checking Multiple Conditions:
+const userLogIn =true;
+const debitCard = true;
+if(userLogIn && debitCard){ // here we uses && opertor means both should be true then if scope will execute.
+    console.log("If the userLogin and debitCard both codition is true then it will execute.")
+}
+
+const logInFromGoogle = false;
+const logInFromEmail = true;
+if(logInFromEmail || logInFromGoogle){ // we use here pipe sign as a OR Operator. Any one will true then if scope execute
+    console.log("If any one of logInFromEmail and logInFromGoogle will be true then if block execute.")
+    console.log("User Logged In")
 }
