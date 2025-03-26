5e2cdf1a48846854f66a764941ac8cd1585340be
ripunjayrai777 <ripunjayrai777@gmail.com>
2025-03-26 17:26:44 +0530

    filter ...lect.29

---
+++ 05_iterations/filter_map_and_reduce.js
@@ -0,0 +1,7 @@
+const coding = ["js", "py", "cpp", "java", "ruby"];
+
+const values = coding.forEach((item)=>{
+    console.log(item);
+});// this is simple forEach loop implementation which are storing in values.
+
+console.log(values);//here this is returning the all data which are store in values after iteration of forEach loop
