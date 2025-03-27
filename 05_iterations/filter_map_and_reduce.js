702132f0ee2386bafbe0088077021a397d0be37f
ripunjayrai777 <ripunjayrai777@gmail.com>
2025-03-27 14:47:01 +0530

    ....

--- 05_iterations/filter_map_and_reduce.js
+++ 05_iterations/filter_map_and_reduce.js
@@ -1,7 +1,11 @@
-const coding = ["js", "py", "cpp", "java", "ruby"];
+// const coding = ["js", "py", "cpp", "java", "ruby"];
 
-const values = coding.forEach((item)=>{
-    console.log(item);
-});// this is simple forEach loop implementation which are storing in values.
+// const values = coding.forEach((item)=>{
+//     console.log(item);
+// });// this is simple forEach loop implementation which are storing in values.
 
-console.log(values);//here this is returning the all data which are store in values after iteration of forEach loop
+// console.log(values);//here this is returning the all data which are store in values after iteration of forEach loop
+// //forEach dont return any value
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
