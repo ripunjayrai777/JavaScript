e7f2f5182e4faffc8e44b322f260e6648f2ebb10
ripunjayrai777 <ripunjayrai777@gmail.com>
2025-03-28 21:47:39 +0530

    ...

--- 05_iterations/filter_map_and_reduce.js
+++ 05_iterations/filter_map_and_reduce.js
@@ -8,4 +8,16 @@
 // //forEach dont return any value
 
 
-const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+// we are going to use here filter and we give callback fuction in the filter
+const newNums = myNums.filter ((num)=> {
+    return num>5 // return keyword is necessary when
+} )
+// we use filter on the myNums array and give the callback function , and stote in to the newNums ..
+// there is one differece btw filter and forEach --filter return some values.. but forEach return nothings
+
+//agar same thing hame forEach me karna ho to..
+
+console.log(newNums);
